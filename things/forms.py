215d6bc6ad400abdaa8110924f5e92dce9b707a3
Neel Nair <neel14nair@gmail.com>
2023-12-05 15:42:01 +0000

    Create form and display in home view

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,14 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
 
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4, 'cols': 15}),  # Customize textarea size
+            'quantity': forms.NumberInput(attrs={'min': 1}),  # Set minimum value for quantity
+        }
+
--- things/views.py
+++ things/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            # Process valid form data, for example, save to the database
+            form.save()
+    else:
+        form = ThingForm()
+
     return render(request, 'home.html', {'form': form})
